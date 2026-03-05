8bdc377c4bec0db3a37dfc71dbc481c445a7c190
Jordan Marr <jordanmarr@users.noreply.github.com>
2026-03-04 20:40:18 -0500

    Added simple publish script

---
+++ publish.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+
+set BUILD_DIR=%~dp0.build
+set TYPEMODEL_PROJ=%~dp0src\FSharp.SourceDjinn.TypeModel\FSharp.SourceDjinn.TypeModel.fsproj
+set DJINN_PROJ=%~dp0src\FSharp.SourceDjinn\FSharp.SourceDjinn.fsproj
+
+echo Cleaning .build folder...
+if exist "%BUILD_DIR%" rd /s /q "%BUILD_DIR%"
+mkdir "%BUILD_DIR%"
+
+echo Packing TypeModel...
+dotnet pack "%TYPEMODEL_PROJ%" -c Release /p:IsPack=true -o "%BUILD_DIR%"
+if errorlevel 1 goto :fail
+
+echo Packing Djinn...
+dotnet pack "%DJINN_PROJ%" -c Release /p:IsPack=true -o "%BUILD_DIR%"
+if errorlevel 1 goto :fail
+
+echo.
+echo Packages written to .build\
+dir /b "%BUILD_DIR%\*.nupkg"
+goto :end
+
+:fail
+echo Build failed.
+exit /b 1
+
+:end
+endlocal
